4e7680ba4d38220ed28cde99656722c61d00f961
“Kezia <keha0001@stud.kea.dk>
2023-08-24 10:50:28 +0200

    Ændret overskrifter og p'er

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const h1 = document.querySelector("h1");
 console.log(h1.textContent);
-h1.innerHTML = "Overskrift";
+h1.textContent = "Overskrift";
 
 const h2 = document.querySelector("h2");
 console.log(h2.textContent);
-h2.innerHTML = "h2 tekst";
+h2.textContent = "h2 tekst";
 
 const h3 = document.querySelector("h3");
 console.log(h3.textContent);
-h3.innerHTML = "h3 tekst ændret";
+h3.textContent = "h3 tekst ændret";
+
+const h33 = document.querySelector("article:nth-child(2) h3");
+console.log(h33.textContent);
+h33.textContent = "h3 tekst ændret";
 
 const p = document.querySelector("p");
 console.log(p.textContent);
-p.innerHTML = "Placeholder tekst bla bla bla";
+p.textContent = "Placeholder tekst bla bla bla";
+
+const p2 = document.querySelector("article:nth-child(2) p");
+console.log(p2.textContent);
+p2.innerHTML = "Placeholder tekst bla bla bla";
 
 const img = document.querySelector("img");
 console.log(img);
