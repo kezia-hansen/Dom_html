5144d77e584e07930c8f7266cfbe4649d48bd685
“Kezia <keha0001@stud.kea.dk>
2023-08-25 12:34:16 +0200

    lavede en bog

---
+++ book.js
@@ -0,0 +1,10 @@
+const firstBook = {
+  title: "Ringenes herre",
+  author: "Tolkien",
+  genres: ["Fantasy", "Drama", "Eventur"],
+  pages: 751,
+  hasRead: true,
+  hasIllustrations: false,
+};
+
+console.log(firstBook.genres);
